69fcb7734bddeafbacb7cbc33be5679c8e1b4100
andres-santiagoperez-dev <andresperez9276@gmail.com>
2026-05-21 10:14:32 -0500

    feat(data): enable data binding for DataGridViews across modules

---
+++ medistock-farmacia/DataAccess/Proveedores.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace medistock_farmacia.DataAccess
+{
+    internal class Proveedores
+    {
+        public static SqlConnection cnn = Conexion.obtenerConexion();
+        public static DataTable obtenerProveedores()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "select top 100 * from categorias;";
+                SqlDataAdapter data = new SqlDataAdapter(sql, cnn);
+                data.Fill(dt);
+                return dt;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "ERROR");
+                return dt;
+            }
+
+        }
+    }
+}
